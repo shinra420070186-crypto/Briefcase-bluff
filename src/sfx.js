8c6ab168ce62707fba9202772fa9fd2dd386ad4d
shinra420070186-crypto <shinra420070186@gmail.com>
2026-03-24 21:29:16 +0530

    Update sfx.js

--- src/sfx.js
+++ src/sfx.js
@@ -1,5 +1,5 @@
 class ProceduralAudioEngine {
-  constructor() { this.ctx = null; }
+  constructor() { this.ctx = null; this.heartbeatInterval = null; }
 
   init() {
     if (!this.ctx) this.ctx = new (window.AudioContext || window.webkitAudioContext)();
@@ -21,63 +21,73 @@ class ProceduralAudioEngine {
 
     osc.connect(gain);
     gain.connect(this.ctx.destination);
-    
     osc.start();
     osc.stop(this.ctx.currentTime + duration);
   }
 
-  // Refined, premium soft click
+  _playNoise(duration, vol, type = 'highpass') {
+    if (!this.ctx) return;
+    const bufferSize = this.ctx.sampleRate * duration;
+    const buffer = this.ctx.createBuffer(1, bufferSize, this.ctx.sampleRate);
+    const data = buffer.getChannelData(0);
+    for (let i = 0; i < bufferSize; i++) data[i] = Math.random() * 2 - 1;
+
+    const noise = this.ctx.createBufferSource();
+    noise.buffer = buffer;
+    const filter = this.ctx.createBiquadFilter();
+    filter.type = type;
+    filter.frequency.value = 1000;
+    const gain = this.ctx.createGain();
+
+    gain.gain.setValueAtTime(vol, this.ctx.currentTime);
+    gain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + duration);
+
+    noise.connect(filter);
+    filter.connect(gain);
+    gain.connect(this.ctx.destination);
+    noise.start();
+  }
+
   tap() { this._playTone(700, 'sine', 0.04, 0.15); }
-  
-  // The satisfying name entry sounds - Untouched
   addPlayer() { this._playTone(440, 'sine', 0.15, 0.4); setTimeout(() => this._playTone(660, 'sine', 0.2, 0.4), 100); }
   removePlayer() { this._playTone(660, 'triangle', 0.15, 0.4); setTimeout(() => this._playTone(440, 'triangle', 0.2, 0.4), 100); }
   
-  begin() {
-    if (!this.ctx) return;
-    const osc = this.ctx.createOscillator();
-    const gain = this.ctx.createGain();
-    const filter = this.ctx.createBiquadFilter();
-
-    osc.type = 'sawtooth';
-    osc.frequency.setValueAtTime(150, this.ctx.currentTime);
-    osc.frequency.exponentialRampToValueAtTime(40, this.ctx.currentTime + 1.5);
-
-    filter.type = 'lowpass';
-    filter.frequency.setValueAtTime(2000, this.ctx.currentTime);
-    filter.frequency.exponentialRampToValueAtTime(100, this.ctx.currentTime + 1.5);
-
-    gain.gain.setValueAtTime(0, this.ctx.currentTime);
-    gain.gain.linearRampToValueAtTime(0.6, this.ctx.currentTime + 0.1);
-    gain.gain.exponentialRampToValueAtTime(0.01, this.ctx.currentTime + 1.5);
-
-    osc.connect(filter);
-    filter.connect(gain);
-    gain.connect(this.ctx.destination);
-
-    osc.start();
-    osc.stop(this.ctx.currentTime + 1.5);
-  }
-
-  // Heavy mechanical double-click for the briefcase latch
-  latchOpen() { 
-    this._playTone(60, 'square', 0.05, 0.7); 
-    setTimeout(() => this._playTone(180, 'sawtooth', 0.03, 0.4), 30); 
-  }
-  latchClose() { 
-    this._playTone(180, 'sawtooth', 0.03, 0.4); 
-    setTimeout(() => this._playTone(60, 'square', 0.05, 0.7), 20); 
-  }
+  // Pneumatic Hiss
+  hiss() { this._playNoise(0.5, 0.3, 'highpass'); }
   
-  timerTick() { this._playTone(1000, 'triangle', 0.1, 0.2); }
-  timerUrgent() { this._playTone(1200, 'square', 0.1, 0.3); }
-  steal() { this._playTone(300, 'sawtooth', 0.5, 0.4, 800); }
-  leave() { this._playTone(800, 'sine', 0.5, 0.4, 300); }
-  
-  gameOver() {
-    if (!this.ctx) return;
-    [300, 380, 450].forEach(freq => this._playTone(freq, 'sawtooth', 2.0, 0.3, freq / 2));
+  // Metallic Slam
+  slam() { 
+    this._playTone(60, 'square', 0.1, 0.8); 
+    this._playTone(120, 'sawtooth', 0.1, 0.5);
+    this._playNoise(0.2, 0.4, 'lowpass'); 
   }
+
+  startHeartbeat() {
+    this.stopHeartbeat();
+    const playBeat = () => {
+      this._playTone(50, 'sine', 0.2, 0.8);
+      setTimeout(() => this._playTone(55, 'sine', 0.2, 0.6), 200);
+    };
+    playBeat();
+    this.heartbeatInterval = setInterval(playBeat, 1000);
+  }
+
+  setHeartbeatSpeed(speedMultiplier) {
+    if (this.heartbeatInterval) {
+      clearInterval(this.heartbeatInterval);
+      const playBeat = () => {
+        this._playTone(50, 'sine', 0.15, 0.8);
+        setTimeout(() => this._playTone(55, 'sine', 0.15, 0.6), 150);
+      };
+      this.heartbeatInterval = setInterval(playBeat, 1000 / speedMultiplier);
+    }
+  }
+
+  stopHeartbeat() {
+    if (this.heartbeatInterval) clearInterval(this.heartbeatInterval);
+  }
+
+  gameOver() { [300, 380, 450].forEach(freq => this._playTone(freq, 'sawtooth', 2.0, 0.3, freq / 2)); }
 }
 
 export const sfx = new ProceduralAudioEngine();
